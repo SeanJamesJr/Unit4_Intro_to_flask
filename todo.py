636b3b906ebb7df70c3482e3c888ef2a0547ce26
sean <sean>
2024-01-02 10:50:43 -0500

    hi

--- todo.py
+++ todo.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask import request 
+from flask import redirect
+
 
 
 app = Flask(__name__)
+todos_list=['own a popeyes', 'own a kfc']
 
 @app.route("/", methods=['GET','POST'])
 def index():
-    new_todo = request.form["new_todo"]
-    todos_list.append(new_todo)
+    if request.method == 'POST':
+        new_todo = request.form["new_todo"]
+        todos_list.append(new_todo)
+    
     return render_template("todo.html.jinja",todos_list=todos_list)
 
-todos_list=['own a popeyes', 'own a kfc']
+
+@app.route('/delete_todo/<int:todo_index>',methods=['POST'])
+def todo_delete(todo_index):
+   del todos_list[todo_index]
+   return redirect('/')
